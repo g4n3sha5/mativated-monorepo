f4c217bea38d251f686ae02a3924f5683264aa4a
g4n3sha5 <kmatysiak-it@outlook.com>
2024-02-06 13:59:02 +0100

    init setup + routing + navbar + hero section

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,6 @@
 import { Outlet } from 'react-router-dom';
-<<<<<<< HEAD
 import { Navbar } from '@/components/common/navbar/';
 import { Footer } from '@/components/common/footer/';
-=======
-import Navbar from 'components/Navbar';
-import Footer from 'components/Footer';
->>>>>>> d94b541 (init)
 
 export default function Layout() {
   return (
