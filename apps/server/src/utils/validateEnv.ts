1229727d1a6e14ce861f281f64c5d066c99021ca
g4n3sha5 <kmatysiak-it@outlook.com>
2024-05-13 14:44:30 +0200

    env variables loading testing

--- apps/server/src/utils/validateEnv.ts
+++ apps/server/src/utils/validateEnv.ts
@@ -1,7 +1,10 @@
 import { cleanEnv, port, str } from 'envalid';
 import dotenv from 'dotenv';
 
-require('dotenv').config();
+// require('dotenv').config();
+
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 export const ValidateEnv = () => {
   cleanEnv(process.env, {
